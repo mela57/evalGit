7cbc85d63638e92debefbae0f6e6b89092a568dc
Gregory WOLFF <gregory.wolff.dev@gmail.com>
2025-03-10 16:06:47 +0100

    Feat : Add calculator fonctionnality

---
+++ script.js
@@ -0,0 +1,92 @@
+let nb1 = "";
+let nb2 = "";
+let res = "";
+let resModulo = "";
+let operateur = "";
+
+const inputNb1 = document.getElementById("nb1");
+const inputNb2 = document.getElementById("nb2");
+const inputOperateur = document.getElementById("operateur");
+const inputRes = document.getElementById("res");
+
+const buttons = {
+    plus: document.getElementById("plus"),
+    moins: document.getElementById("moins"),
+    mult: document.getElementById("mult"),
+    div: document.getElementById("div"),
+    egal: document.getElementById("egal"),
+    c: document.getElementById("c")
+};
+
+Object.keys(buttons).forEach(id => {
+    if (id !== "egal" && id !== "c") {
+        buttons[id].addEventListener('click', () => {
+            setOperateur(buttons[id].value);
+        });
+    }
+});
+
+
+buttons.egal.addEventListener('click', function () {
+    calculer();
+});
+
+
+buttons.c.addEventListener('click', function () {
+    reset();
+});
+
+function setOperateur(value) {
+    operateur = value;
+    inputOperateur.value = value;
+}
+
+function reset() {
+    inputNb1.value = "";
+    inputNb2.value = "";
+    inputOperateur.value = "";
+    inputRes.value = "";
+    nb1 = "";
+    nb2 = "";
+    operateur = "";
+}
+
+function calculer() {
+    nb1 = inputNb1.value;
+    nb2 = inputNb2.value;
+    
+    if (isNaN(nb1) || isNaN(nb2)) {
+        alert("Veuillez entrer des nombres valides");
+        return;
+    }
+
+    nb1 = parseInt(nb1);
+    nb2 = parseInt(nb2);
+
+    switch (operateur) {
+        case "+":
+            res = nb1 + nb2;
+            break;
+        case "-":
+            res = nb1 - nb2;
+            break;
+        case "x":
+            res = nb1 * nb2;
+            break;
+        case "/":
+            if (nb2 !== 0) {
+                res = nb1 / nb2;
+                resModulo = nb1 % nb2;
+                res = `${res} reste ${resModulo}`;
+            } else {
+                alert("Division par 0 impossible");
+                return;
+            }
+            break;
+        default:
+            alert("Opérateur inconnu");
+            return;
+    }
+    
+    inputRes.value = res;
+}
